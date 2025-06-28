844d11b65c9295ab94aabff979e1b1840784aad8
GrowwStacks2023 <admin@growwstacks.com>
2025-06-28 17:13:48 +0530

    test

--- database.py
+++ database.py
@@ -3,8 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import user, password, database
 
-# Updated for Azure PostgreSQL
-DATABASE_URL = f'postgresql://{user}:{password}@hr-growwstacks-server.postgres.database.azure.com:5432/{database}?sslmode=require'
+
+# Replace with your own MySQL credentials
+DATABASE_URL = f'mysql+pymysql://{user}:{password}@localhost:3306/{database}'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
