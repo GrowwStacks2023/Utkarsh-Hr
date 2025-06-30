96556f9d4c52ac1bee2ec08ece3baff1a954835b
GrowwStacks2023 <154306310+GrowwStacks2023@users.noreply.github.com>
2025-06-30 21:57:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,5 +25,5 @@ vapi_key = os.getenv('VAPI_KEY', '0126f44e-2158-4894-a63b-ce12c885741e')
 vapi_assistant_id = os.getenv('VAPI_ASSISTANT_ID', 'fd1d82a1-7685-4dce-8608-216be650e349')
 
 # API key
-op_api_key = os.getenv('OPENAI_API_KEY', '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
+op_api_key = os.getenv('OPENAI_API_KEY', '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
  ')
