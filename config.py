0794494a8370207f6b7c37bac590b4ac536138fc
GrowwStacks2023 <154306310+GrowwStacks2023@users.noreply.github.com>
2025-06-30 23:03:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,6 +23,6 @@ base_url = os.getenv('BASE_URL', 'https://testgroww-embjdjbpawf4hpat.canadacentr
 vapi_key = os.getenv('VAPI_KEY', '0126f44e-2158-4894-a63b-ce12c885741e')
 vapi_assistant_id = os.getenv('VAPI_ASSISTANT_ID', 'fd1d82a1-7685-4dce-8608-216be650e349')
 
-# API key
-op_api_key = os.getenv('OPENAI_API_KEY', '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
- ')
+# api key
+op_api_key = os.getenv('op_api_key',"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")
+op_api_project_id = os.getenv('op_api_project_id',"proj_fXgqilFEvkf2BSIE8ivh2TpG")
